567ee5480add71cc03869e5fb254a0f5e7bf3b34
CodeFuller <CodeFuller@gmail.com>
2021-04-07 18:11:08 +0300

    Add skeleton of DemoServer

---
+++ src/DemoServer/Program.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using CodeFuller.Library.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DemoServer
+{
+	public static class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			await CreateHostBuilder(args).Build().RunAsync();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				})
+				.ConfigureLogging((hostingContext, loggingBuilder) =>
+				{
+					loggingBuilder.ClearProviders();
+					loggingBuilder.AddLogging(settings => hostingContext.Configuration.Bind("logging", settings));
+				});
+	}
+}
---
+++ src/DemoServer/Startup.cs
@@ -0,0 +1,44 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DemoServer
+{
+	public class Startup
+	{
+		private readonly IConfiguration configuration;
+
+		public Startup(IConfiguration configuration)
+		{
+			this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+		}
+
+#pragma warning disable CA1801 // Remove unused parameter
+		public void ConfigureServices(IServiceCollection services)
+#pragma warning restore CA1801 // Remove unused parameter
+		{
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapGet("/", async context =>
+				{
+					await context.Response.WriteAsync("Hello World!");
+				});
+			});
+		}
+	}
+}
